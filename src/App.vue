a15de9617d4365aa66ade871ef1dfa55465f5667
zz <1871490331@qq.com>
2022-06-28 00:38:44 +0800

    提交时的注释

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- 路由的出口 -->
+    <router-view></router-view>
+    <h1>黑马头条</h1>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "ToutiaoMApp",
 
-#nav {
-  padding: 30px;
+  data() {
+    return {};
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  mounted() {},
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  methods: {},
+};
+</script>
+
+<style scoped>
 </style>
