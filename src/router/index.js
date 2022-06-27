2ba4326043718e23da67f1847d3977c9c181fda1
zz <1871490331@qq.com>
2022-06-28 00:59:37 +0800

    项目初始化-引入vant组件库

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 // 引入全局less文件
 import "@/styles/index.less";
+// 引入vant
+import Vant from "vant";
+// 引入vant的css文件
+import "vant/lib/index.css";
+
+Vue.use(Vant);
 Vue.use(VueRouter);
 
 const routes = [];
