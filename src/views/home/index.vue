ce0e16a22e505b08b5a1bdd6fcb20d52bd0d535d
zz <1871490331@qq.com>
2022-07-08 01:05:22 +0800

    完成了首页Tab导航栏搭建以及Tab切换栏的初始引入

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- nav标题导航栏S -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        slot="title"
+        size="small"
+        round
+        icon="search"
+        type="info"
+        class="search-btn"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+    <!-- nav标题导航栏E -->
+    <!-- Tab标签页S -->
+    <van-tabs v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 6">内容 6</van-tab>
+    </van-tabs>
+    <!-- Tab标签页E -->
+  </div>
 </template>
 
 <script>
@@ -7,7 +31,10 @@ export default {
   name: "HomeIndex",
 
   data() {
-    return {};
+    return {
+      // 通过名称匹配在标签指定 name 属性的情况下，v-model 的值为当前标签的 name（此时无法通过索引值来匹配标签）。
+      active: 0,
+    };
   },
 
   mounted() {},
@@ -16,5 +43,21 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style  lang="less" >
+.home-container {
+  .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 6.9375rem;
+    height: 0.8rem;
+    background-color: #5babfb;
+    border: none;
+    font-size: 0.35rem;
+    .van-icon {
+      color: #fff;
+      font-size: 0.4rem;
+    }
+  }
+}
 </style>
