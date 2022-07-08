cd974e5885948fc9a5f96bba2a31aa4e4138dbee
zz <1871490331@qq.com>
2022-07-08 13:09:51 +0800

    完成了头部导航栏切换的样式搭建

---
+++ src/components/SearchTab.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- nav标题导航栏S -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        slot="title"
+        size="small"
+        round
+        icon="search"
+        type="info"
+        class="search-btn"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+    <!-- nav标题导航栏E -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToutiaoMSearchTab",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="home-container">
     <!-- nav标题导航栏S -->
-    <van-nav-bar class="page-nav-bar">
-      <van-button
-        slot="title"
-        size="small"
-        round
-        icon="search"
-        type="info"
-        class="search-btn"
-        >搜索</van-button
-      >
-    </van-nav-bar>
+    <SearchTab></SearchTab>
     <!-- nav标题导航栏E -->
     <!-- Tab标签页S -->
-    <van-tabs v-model="active" animated swipeable>
+    <van-tabs v-model="active" animated swipeable class="channel-tab">
       <van-tab title="标签 1">内容 1</van-tab>
       <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
@@ -27,8 +17,11 @@
 </template>
 
 <script>
+import SearchTab from "@/components/SearchTab.vue";
+
 export default {
   name: "HomeIndex",
+  components: { SearchTab },
 
   data() {
     return {
@@ -36,27 +29,48 @@ export default {
       active: 0,
     };
   },
-
   mounted() {},
 
   methods: {},
 };
 </script>
 
-<style  lang="less" >
+<style  lang="less" scoped>
 .home-container {
-  .van-nav-bar__title {
+  // ::v-deep 或者 /deep/  原因是由于我们在作用子组件的样式，如果在一个作用域组件样式当中，默认只会影响作用到组件的根节点上，如果想要作用的更深，要影响到后代元素，必须加上深度操作符号在所需样式前加::v-deep 或者 /deep/或者>>>
+
+  ::v-deep .van-nav-bar__title {
     max-width: unset;
+    .search-btn {
+      width: 6.9375rem;
+      height: 0.8rem;
+      background-color: #5babfb;
+      border: none;
+      font-size: 0.35rem;
+      .van-icon {
+        color: #fff;
+        font-size: 0.4rem;
+      }
+    }
   }
-  .search-btn {
-    width: 6.9375rem;
-    height: 0.8rem;
-    background-color: #5babfb;
-    border: none;
-    font-size: 0.35rem;
-    .van-icon {
-      color: #fff;
-      font-size: 0.4rem;
+  /deep/.channel-tab {
+    .van-tabs__wrap {
+      height: 1rem;
+    }
+    .van-tab {
+      min-width: 2.5rem;
+      border-right: 1px solid #edeff3;
+      color: #777;
+    }
+    .van-tab--active {
+      // background-color: #3296fa;
+      color: #333;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      width: 0.3875rem;
+      height: 0.075rem;
+      margin-bottom: 0.1125rem;
     }
   }
 }
